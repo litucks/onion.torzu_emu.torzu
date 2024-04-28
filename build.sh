b867a4e7bb57bd37e2523054b92116fbcbb7d4cc
spectranator <spectranator@y2nlvhmmk5jnsvechppxnbyzmmv3vbl7dvzn6ltwcdbpgxixp3clkgqd.onion>
2024-04-29 00:02:51 +0200

    Updated more files for working build

---
+++ build.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+flatpak-builder build onion.torzu_emu.torzu.json &&
+flatpak build-export export build &&
+exec flatpak build-bundle export torzu.flatpak onion.torzu_emu.torzu --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
