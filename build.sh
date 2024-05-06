93c0281b24b69020def30650a692a02f5589487e
spectranator <spectranator@y2nlvhmmk5jnsvechppxnbyzmmv3vbl7dvzn6ltwcdbpgxixp3clkgqd.onion>
2024-05-07 00:00:56 +0200

    Clean up everything properly before build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+rm -rf build export .flatpak-builder &&
 flatpak-builder build onion.torzu_emu.torzu.json &&
 flatpak build-export export build &&
 exec flatpak build-bundle export torzu.flatpak onion.torzu_emu.torzu --runtime-repo=https://flathub.org/repo/flathub.flatpakrepo
